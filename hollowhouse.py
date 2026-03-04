34021d46af5497448589e6bffd467ddd7217e6f4
Jinnqs <mlwhit5@ilstu.edu>
2026-03-04 16:00:08 -0600

    Create hollowhouse.py

---
+++ hollowhouse.py
@@ -0,0 +1,16 @@
+# Import the api
+from mcpi_addons.minecraft import Minecraft
+# Initialize the api (MPCI must be open and in a world at this time)
+mc = Minecraft.create()
+
+length = 10
+height = 10
+width = 10
+
+x = int(input("X Coordinate: "))
+y = int(input("Y Coordinate: "))
+z = int(input("Z Coordinate: "))
+
+mc.setBlocks(x, y, z, x + width, y + height, z + length, 3)
+mc.setBlocks(x + 1, y, z+1, x + width - 1, y + height - 1, z + length - 1, 0)
+             
